2f54ef9fa0c40617fc61e11e915c0fc5703f50cd
Alan Zhou <hauy@163.com>
2023-12-26 09:20:03 +0800

    feat: modify site name

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,8 +6,8 @@ import { getClientConfig } from "./config/client";
 import { type Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "NextChat",
-  description: "Your personal ChatGPT Chat Bot.",
+  title: "JoyceAI",
+  description: "Powered by OpenAI",
   viewport: {
     width: "device-width",
     initialScale: 1,
